02e785ffa04e52f2e0918f4874e326fc4ccb9cde
samumasa <masahira0902@gmail.com>
2025-04-05 16:59:57 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const addBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    text.textContent = "ボタンをクリックしました";
+  }, 2000);
+});
