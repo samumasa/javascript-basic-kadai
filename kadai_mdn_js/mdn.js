968104ef63ac55ff92edd9fc6e7adc5a05a329c1
samumasa <masahira0902@gmail.com>
2025-04-05 15:13:53 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const date = new Date();
+
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+const formattedDate = date.toLocaleDateString('ja-JP', options);
+console.log(formattedDate);
+
